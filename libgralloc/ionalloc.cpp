8d2a706bc9efec8ac1cf436e0d9e7b0756811787
Craig Tatlor <ctatlor97@gmail.com>
2018-05-23 09:04:35 +0000

    display-caf: msm8974: Changes for VNDK_VERSION=current Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- libgralloc/ionalloc.cpp
+++ libgralloc/ionalloc.cpp
@@ -82,6 +82,11 @@ int IonAlloc::alloc_buffer(alloc_data& data)
     if (data.flags & ION_SECURE)
         ionAllocData.flags |= ION_SECURE;
 
+    // ToDo: replace usage of alloc data structure with
+    //  ionallocdata structure.
+    if (data.flags & ION_SECURE)
+        ionAllocData.flags |= ION_SECURE;
+
     err = open_device();
     if (err)
         return err;
@@ -179,7 +184,6 @@ int IonAlloc::clean_buffer(void *base, size_t size, int offset, int fd, int op)
     struct ion_flush_data flush_data;
     struct ion_fd_data fd_data;
     struct ion_handle_data handle_data;
-    ion_user_handle_t handle;
     int err = 0;
 
     err = open_device();
